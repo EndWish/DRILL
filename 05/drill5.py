c22608f7a7408299104471cc3d033132732f2f71
EndWish <90082921+EndWish@users.noreply.github.com>
2021-09-13 15:01:02 +0900

    DRILL #5. 거북이 조종하기
    2018180009 김시인

---
+++ 05/drill5.py
@@ -0,0 +1,37 @@
+import turtle
+
+def go():
+    turtle.stamp()
+    turtle.forward(50)
+
+def move_up():
+    turtle.setheading(90)
+    go()
+
+def move_down():
+    turtle.setheading(270)
+    go()
+
+def move_right():
+    turtle.setheading(0)
+    go()
+
+def move_left():
+    turtle.setheading(180)
+    go()
+
+def restart():
+    turtle.reset()
+
+turtle.shape('turtle')
+
+turtle.onkey(move_up, 'W')
+turtle.onkey(move_up, 'w')
+turtle.onkey(move_down, 'S')
+turtle.onkey(move_down, 's')
+turtle.onkey(move_left, 'A')
+turtle.onkey(move_left, 'a')
+turtle.onkey(move_right, 'D')
+turtle.onkey(move_right, 'd')
+turtle.onkey(restart, 'Escape')
+turtle.listen()
